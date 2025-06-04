808399e9bfc8600d9b5347b05c3edadaf7eb55c1
err0rgod <ndgaming458@gmail.com>
2025-06-04 18:18:55 +0530

    added documentation in the code

--- subenum.py
+++ subenum.py
@@ -1,9 +1,9 @@
-import requests
-from concurrent.futures import ThreadPoolExecutor
-import argparse
-from tqdm import tqdm
-from colorama import Fore, Style, init
-import urllib3
+import requests   #for requests library to send http requests
+from concurrent.futures import ThreadPoolExecutor #for multithreading
+import argparse #for command line argument parsing
+from tqdm import tqdm #for progress bar
+from colorama import Fore, Style, init #for colored output in terminal
+import urllib3 #for disabling SSL warnings
 
 print(r"          __________________     _   _______  ____   ____   __ _/")
 print(r"         _/ __ \_  __ \_  __ \/  /_\  \_  __ \/ ___\ /  _ \ / __ | ")
@@ -14,12 +14,20 @@ print(r"             \/                     \/     /_____/             \/ ")
 print("Subenum V1.0 - Subdomain Enumeration Tool")
 print("Author: err0rgod")
 
+#ASCCI art and tool information
+
+#Initialize colorama for colored output and supress SSL warnings
+
 init(autoreset=True)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# loading subdoms from a file and loading in to List
 
 def load_subdomains(filename):
     with open(filename, 'r') as f:
         return [line.strip() for line in f if line.strip()]
+
+#Checking if the subdomain is live or dead by Head request and returns a bool value
 
 def check_subdomain(full_domain):
     try:
@@ -27,6 +35,8 @@ def check_subdomain(full_domain):
         return full_domain, response.status_code < 400
     except Exception:
         return full_domain, False
+
+#Main function initializes on start ask user for requires info by -f -d -t -o -nc arguments
 
 def main():
     parser = argparse.ArgumentParser(description="Subdomain checker")
@@ -57,9 +67,11 @@ def main():
 
     subdomains = load_subdomains(subdomains_file)
     live_subdomains = []
-
+#check for the full subdomains by appending subdomains with main domain
     print("\nTesting subdomains...\n")
     full_domains = [f"{sub}.{domain}" for sub in subdomains]
+
+#Using multithreading for faster performance but it will use more resources.
 
     with ThreadPoolExecutor(max_workers=args.threads) as executor:
         results = list(tqdm(executor.map(check_subdomain, full_domains), total=len(full_domains), desc="Overall Progress"))
@@ -76,10 +88,16 @@ def main():
                 else:
                     print(f"[DEAD] {full_domain}")
 
+
+#Writing the subdomains to the output file 
+
     with open(args.output, 'w') as f:
         for live in live_subdomains:
             f.write(live + "\n")
     print(f"\nLive subdomains saved to {args.output}")
+
+
+   #calling the main f(x) to start the program 
 
 if __name__ == "__main__":
     main()
